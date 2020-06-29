76f4e188151fb5691f00eed3ce98ad93fa22461d
Zo3i <30296801+Zo3i@users.noreply.github.com>
2020-06-30 00:16:07 +0800

    add 反转链表

---
+++ 反转链表/main.js
@@ -0,0 +1,29 @@
+/*
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL */
+
+var reverseList = function(head) {
+   
+   if (head == null) {
+       return null
+   }
+   
+   let pre = head
+   let cur = head.next
+   let temp
+   
+   while (cur != null) {
+       temp = cur.next
+       cur.next = pre
+       pre = cur
+       cur = temp
+   }
+   
+   head.next = null
+   
+   return pre
+};
